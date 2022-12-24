da5b26789f279b2c25650fafe1154689accf2e72
mikhailzubov <mikhailzubov88@gmail.com>
2022-12-24 18:17:42 +0300

    Поправил кнопки на главной странице

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,39 +1,50 @@
-import React from 'react'
-import classes from "./Main.module.scss"
-import ButtonText from '../../components/UI/buttons/ButtonText/ButtonText'
-import bubbles from '../../assets/images/bubbles.png'
-import backdrop from '../../assets/images/backdrop.png'
-import Header from '../../components/Header/Header'
-import InfoSection from '../../components/sections/InfoSection/InfoSection'
-import ButtonBig from '../../components/UI/buttons/ButtonBig/ButtonBig'
-import PageTitles from '../../components/UI/Textual/PageTitles/PageTitles'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import classes from "./Main.module.scss";
+import ButtonText from "../../components/UI/buttons/ButtonText/ButtonText";
+import bubbles from "../../assets/images/bubbles.png";
+import backdrop from "../../assets/images/backdrop.png";
+import Header from "../../components/Header/Header";
+import InfoSection from "../../components/sections/InfoSection/InfoSection";
+import ButtonBig from "../../components/UI/buttons/ButtonBig/ButtonBig";
+import PageTitles from "../../components/UI/Textual/PageTitles/PageTitles";
 
 export default function Main() {
   return (
     <>
-        <Header />
-        <main className={classes.Main}>
-            <img src={backdrop} alt="" className={classes.Main__bgBackdrop}/>
-            <article className={classes.Main__inner}>
-                <img src={bubbles} alt="" className={classes.Main__bgBubbles}/>
-                <PageTitles style={{marginBottom: '2.5vw'}} title='Транспорт близко' subtitle='ищите транспорт МГТ по бортовому номеру'/>
-                <ButtonText title="Опробовать"/>
-                <article className={classes.Main__info}>
-                    <InfoSection title="Что это?"
-                     text="МГТПарсер - это набор инструментов для отслеживания транспортных средств Московского городского транспорта"
-                     />
-                    <InfoSection title="Подробнее?"
-                     text="Вы можете отследить, например автобус, по его бортовому номеру, или посмотреть все автобусы на определённом маршруте"
-                     />
-                </article>
-                <article className={classes.Main__action}>
-                    <h3 className={classes.Main__actionTitle}>
-                        Начните сейчас
-                    </h3>
-                    <ButtonBig title="Начать триал"/>
-                </article>
-            </article>
-        </main>
+      <Header />
+      <main className={classes.Main}>
+        <img src={backdrop} alt="" className={classes.Main__bgBackdrop} />
+        <article className={classes.Main__inner}>
+          <img src={bubbles} alt="" className={classes.Main__bgBubbles} />
+          <PageTitles
+            style={{ marginBottom: "2.5vw" }}
+            title="Транспорт близко"
+            subtitle="ищите транспорт МГТ по бортовому номеру"
+          />
+          <Link to="/parser">
+            <ButtonText title="Опробовать" />
+          </Link>
+          <article className={classes.Main__info}>
+            <InfoSection
+              title="Что это?"
+              text="МГТПарсер - это набор инструментов для отслеживания транспортных средств Московского городского транспорта"
+            />
+            <InfoSection
+              title="Подробнее?"
+              text="Вы можете отследить, например автобус, по его бортовому номеру, или посмотреть все автобусы на определённом маршруте"
+            />
+          </article>
+          <article className={classes.Main__action}>
+            <h3 className={classes.Main__actionTitle}>Начните сейчас</h3>
+
+            <Link to="/parser">
+              <ButtonBig title="Начать триал" />
+            </Link>
+          </article>
+        </article>
+      </main>
     </>
-  )
+  );
 }
